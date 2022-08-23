5648cef96cb13bc74e8e90339cd5ccfadd5b5b04
almenoliver02@gmail.com <almenoliver02@gmail.com>
2022-08-23 16:01:58 +0200

    Added seed data from example

--- Gymbokning/Program.cs
+++ Gymbokning/Program.cs
@@ -11,12 +11,35 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// SeedData
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var db = services.GetRequiredService<ApplicationDbContext>();
+
+    db.Database.EnsureDeleted();
+    db.Database.Migrate();
+    var config = services.GetRequiredService<IConfiguration>();
+    var adminPW = config["AdminPW"];
+
+    try
+    {
+        SeedData.InitAsync(db, services, adminPW).GetAwaiter().GetResult();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+        throw;
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
